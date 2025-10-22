40411fa0610b944a3e2b4028af7914de79591964
tahreem7 <tahreemhossain8@gmail.com>
2025-10-22 19:09:33 +0600

    Sponsors Section Added

--- src/components/banner/sponsors/index.tsx
+++ src/components/banner/sponsors/index.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 function SponsorCard() {
   return (
-    <div>SponsorCard</div>
-  )
+    <section className="max-w-7xl mx-auto flex justify-center py-8 gap-18">
+      <Image
+        src="/sponsors/1.png" 
+        alt="Sponsor Logo"
+        width={40}
+        height={40}
+        className="object-contain"
+      />
+      <Image
+        src="/sponsors/2.png" 
+        alt="Sponsor Logo"
+        width={40}
+        height={40}
+        className="object-contain"
+      />
+      <Image
+        src="/sponsors/3.png" 
+        alt="Sponsor Logo"
+        width={40}
+        height={40}
+        className="object-contain"
+      />
+      <Image
+        src="/sponsors/4.png" 
+        alt="Sponsor Logo"
+        width={40}
+        height={40}
+        className="object-contain"
+      />
+      <Image
+        src="/sponsors/5.png" 
+        alt="Sponsor Logo"
+        width={40}
+        height={40}
+        className="object-contain"
+      />
+      <Image
+        src="/sponsors/6.png" 
+        alt="Sponsor Logo"
+        width={40}
+        height={40}
+        className="object-contain"
+      />
+    </section>
+  );
 }
 
-export default SponsorCard
+export default SponsorCard;
